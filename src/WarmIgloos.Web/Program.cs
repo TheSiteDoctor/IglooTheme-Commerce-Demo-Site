c6b72f060d9a131f0716de457af4051f80824e45
joeyCV93 <Joeycv@hotmail.co.uk>
2025-07-14 11:31:05 +0100

    IT-672 Upload Project

---
+++ src/WarmIgloos.Web/Program.cs
@@ -0,0 +1,34 @@
+
+using Umbraco.Commerce.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddUmbracoCommerce(b => {
+        b.AddStorefrontApi();
+    })
+    .AddComposers()
+    .Build();
+
+var app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
